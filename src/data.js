4eb42cf89414cb1a6999ddb492b1408586161d90
yash8351 <yash8351@gmail.com>
2022-08-02 17:15:28 +0530

    React shopping app

---
+++ src/data.js
@@ -0,0 +1,23 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'Laptop',
+      price: 16000,
+      image: 'https://cdn.pixabay.com/photo/2016/06/08/10/35/laptop-1443559__480.jpg',
+    },
+    {
+      id: '2',
+      name: 'Car',
+      price: 250000,
+      image: 'https://media.istockphoto.com/photos/old-abandoned-vintage-car-wreck-picture-id1165713362',
+    },
+    {
+      id: '3',
+      name: 'Summer Kit',
+      price: 8000,
+      image: 'https://cdn.pixabay.com/photo/2019/04/11/23/21/watercolor-4120925_960_720.png',
+    },
+  ],
+};
+export default data;
